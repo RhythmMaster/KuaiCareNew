f41d1c9b36d3ab8b3cb1117c0da7ddd22ce7f4d3
macbook user <macbookuser@macbookdemacbook-pro.local>
2016-10-24 08:56:12 +0800

    Initial Commit

---
+++ KuaiCare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KuaiCare
+//
+//  Created by macbook user on 16/10/24.
+//  Copyright © 2016年 macbook user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
